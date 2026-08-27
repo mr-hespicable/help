26d434d8f0c560789314f5e4d4f4a3303d9fc181
leonmcqueen@macbook-pro <lhm.fb.2015@gmail.com>
2026-08-27 01:02:52 +0100

    shouldn't be tracking `target/`... oops!

--- compiler/target/debug/deps/compiler-8d49b7278b70e99e.d
+++
@@ -1,5 +0,0 @@
-/Users/porgy/notes/school/compsci/help/compiler/target/debug/deps/compiler-8d49b7278b70e99e.d: src/main.rs
-
-/Users/porgy/notes/school/compsci/help/compiler/target/debug/deps/compiler-8d49b7278b70e99e: src/main.rs
-
-src/main.rs:
